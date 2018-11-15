d59195cce1110cb38a50e83f93c1fffd812d6cd7
James Hwang <91juhwang@gmail.com>
2018-11-15 16:20:56 -0500

    Create db seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,23 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Book.create([
-  { name: 'How to Make Money', }
+  { title: 'How to Make Money', release_date: '2018-02-12', base_price: 123, format_id: 1},
+  { title: 'How to Make Money2', release_date: '2018-02-13', base_price: 123, format_id: 2},
+  { title: 'How to Make Money3', release_date: '2018-02-14', base_price: 123, format_id: 3},
+  { title: 'How to Make Money4', release_date: '2018-02-15', base_price: 123, format_id: 1},
 ])
-Format.create()
-Author.create()
-Archive.create()
+Format.create([
+  {name: 'hardcover'},
+  {name: 'softcover'},
+  {name: 'digital'}
+])
+Author.create([
+  {first_name: 'james', last_name: 'hwang'},
+  {first_name: 'will', last_name: 'smith'},
+  {first_name: 'nooo', last_name: 'mmm'}
+])
+Archive.create([
+  {book_id: 1, author_id: 1},
+  {book_id: 1, author_id: 2},
+  {book_id: 2, author_id: 3},
+])
